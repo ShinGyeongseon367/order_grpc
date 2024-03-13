bc0bf2d2b94d30498acb81b9211a4c3dd0e47962
ShinGyeongseon <tlsrid1119@gmail.com>
2024-03-14 01:08:18 +0900

    엔드포인트 개발

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"log"
+
+	"gitHub.com/ShinGyeongseon367/microservices/order/config"
+	"gitHub.com/ShinGyeongseon367/microservices/order/internal/adapter/db"
+	"gitHub.com/ShinGyeongseon367/microservices/order/internal/adapter/grpc"
+	"gitHub.com/ShinGyeongseon367/microservices/order/internal/application/core/api"
+)
+
+func main() {
+	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
+	if err != nil {
+		log.Fatalf("Failed to connect to database. Error: %v", err)
+	}
+
+	application := api.NewApplication(dbAdapter)
+	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
+	grpcAdapter.Run()
+}
