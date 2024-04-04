993a397fd7132facb34b28b80f32e86145e9da60
ShinGyeongseon <tlsrid1119@gmail.com>
2024-04-05 00:28:31 +0900

    update error message in api

--- internal/application/core/api/api.go
+++ internal/application/core/api/api.go
@@ -1,8 +1,13 @@
 package api
 
 import (
+	"strings"
+
 	"gitHub.com/ShinGyeongseon367/microservices/order/internal/application/core/domain"
 	"gitHub.com/ShinGyeongseon367/microservices/order/internal/ports"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Application struct {
@@ -21,6 +26,31 @@ func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 	err := a.db.Save(&order)
 	if err != nil {
 		return domain.Order{}, err
+	}
+
+	paymentErr := a.paymentAdapter.Charge(&order)
+	if paymentErr != nil {
+		// st, _ := status.FromError(paymentErr)
+		st := status.Convert(paymentErr)
+		var allErrors []string
+		for _, detail := range st.Details() {
+			switch t := detail.(type) {
+			case *errdetails.BadRequest:
+				for _, violation := range t.GetFieldViolations() {
+					allErrors = append(allErrors, violation.Description)
+				}
+			}
+		}
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "payment",
+			Description: strings.Join(allErrors, "\n"),
+		}
+
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+		orderStatus := status.New(codes.InvalidArgument, "order creation failed")
+		statusWithDetail, _ := orderStatus.WithDetails(badReq)
+		return domain.Order{}, statusWithDetail.Err()
 	}
 
 	return order, nil
